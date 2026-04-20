3993ddac0ed48c6a7beaa854166a0031ac16d422
FAIZAN IMRAN <itxfaizanimran786@gmail.com>
2026-04-21 00:21:55 +0500

    Add files via upload

---
+++ video5_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+//alert(hey)  we are using nade js not brower
+
+console.log(2+2)
+console.log("faizan") // code readability should be high
+
+
+
+ let name = "faizan" //string datatype 
+ let age = 14   
+ let isLoggedin = false 
+// numbers datatype  => range  2 to power 53
+//string datatype  => use " "
+// boolean datatype => true/false
+//null => standa lone value
+// undefined =>
+// symbol => uniqueness
+
+
+// object 
+
+console.log(typeof "faizan")
+console.log(typeof age)
+
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
