dea16d859302280065f5e6728246e02f035219d8
FAIZAN IMRAN <itxfaizanimran786@gmail.com>
2026-04-22 02:19:49 +0500

    Add files via upload

---
+++ video7_operation.js
@@ -0,0 +1,50 @@
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+console.log("--------------------------------");
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**4);
+console.log(2/2);
+console.log(2%4);
+console.log("---------------------------------");
+
+
+let value1 = "Hi"
+let value2 = " Faizan"
+let value3 = value1 + value2 
+
+console.log(value3);
+console.log("----------------------------------");
+
+
+
+
+console.log("1" + 2);  // if this type of conversion in js. if string, at first then next all values
+                       // consider as string
+console.log(2 + "1");  //if this type of conversion in js. if number, at first then next all values
+                       // consider as number without string
+console.log(1 + 2 + "1"); 
+console.log("--------------------------------");
+
+
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter)
+
+let gamecounter =111;
+++gamecounter;
+console.log(gamecounter);
+
+
+
+
+
+
+
+
+ 
