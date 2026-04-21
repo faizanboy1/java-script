e9642b566fd40ce24ccda3c0f1257d28bc09bc64
FAIZAN IMRAN <itxfaizanimran786@gmail.com>
2026-04-22 01:13:36 +0500

    Add files via upload

---
+++ video6_conversion_operations.js
@@ -0,0 +1,30 @@
+
+let score = undefined 
+
+console.log(typeof score)
+
+let valueInnumber =Number(score);
+
+console.log(typeof valueInnumber);
+console.log(valueInnumber)
+
+// "33" => 33
+// "33abc" =>NaN (not a number)
+// true => 1
+//false => 0
+
+let isLoggedin ="1"
+
+
+let booleanIsloggedin = Boolean(isLoggedin)
+console.log(booleanIsloggedin)
+
+//in boolean 
+//1 => true ; 0 => false
+// "" => false
+// "faizan" => ture
+
+let number = 22
+let stringNumber =String(number)
+console.log(stringNumber)
+console.log(typeof stringNumber)
