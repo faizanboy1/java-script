4e9c57598769779e9cd1a1cfa1b7e9e0157c4dd5
FAIZAN IMRAN <itxfaizanimran786@gmail.com>
2026-06-05 07:30:14 +0500

    Add files via upload

---
+++ video8_camparion.js
@@ -0,0 +1,22 @@
+// console.log(2>1);
+
+// console.log("2">1);
+// console.log("02"<1);
+// // console.log("1"==1);
+
+
+// console.log(null ==0); this type of comparison not used because they are not reliable
+// console.log(null >=0);
+// console.log(null <=0);
+// console.log(null !=0);
+
+// console.log(undefined == 0);
+// console.log(undefined <= 0);
+// console.log(undefined >= 0);
+// console.log(undefined != 0);
+
+
+//concept of strict comparison
+
+console.log("2"  === 2);
+
